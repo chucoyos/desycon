9271a5e26cd3cc28dfc30b7cdbdb017c06bea8f0
chucoyos <unodev@outlook.com>
2025-12-09 02:59:38 -0600

    fix: restore missing status_icon and status_badge_class methods

--- app/helpers/bl_house_lines_helper.rb
+++ app/helpers/bl_house_lines_helper.rb
@@ -1,2 +1,51 @@
 module BlHouseLinesHelper
+  def status_badge_class(status)
+    case status
+    when "activo"
+      "bg-green-100 text-green-800"
+    when "bl_original"
+      "bg-blue-100 text-blue-800"
+    when "documentos_ok"
+      "bg-yellow-100 text-yellow-800"
+    when "despachado"
+      "bg-purple-100 text-purple-800"
+    when "pendiente_endoso_agente_aduanal"
+      "bg-orange-100 text-orange-800"
+    when "pendiente_endoso_consignatario"
+      "bg-red-100 text-red-800"
+    when "finalizado"
+      "bg-gray-100 text-gray-800"
+    when "instrucciones_pendientes"
+      "bg-indigo-100 text-indigo-800"
+    when "pendiente_pagos_locales"
+      "bg-pink-100 text-pink-800"
+    else
+      "bg-gray-100 text-gray-800"
+    end
+  end
+
+  def status_icon(status)
+    case status
+    when "activo"
+      '<svg class="w-4 h-4" fill="none" stroke="currentColor" viewBox="0 0 24 24"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 13l4 4L19 7"/></svg>'.html_safe
+    when "bl_original"
+      '<svg class="w-4 h-4" fill="none" stroke="currentColor" viewBox="0 0 24 24"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 12h6m-6 4h6m2 5H7a2 2 0 01-2-2V5a2 2 0 012-2h5.586a1 1 0 01.707.293l5.414 5.414a1 1 0 01.293.707V19a2 2 0 01-2 2z"/></svg>'.html_safe
+    when "documentos_ok"
+      '<svg class="w-4 h-4" fill="none" stroke="currentColor" viewBox="0 0 24 24"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z"/></svg>'.html_safe
+    when "despachado"
+      '<svg class="w-4 h-4" fill="none" stroke="currentColor" viewBox="0 0 24 24"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M20 7l-8-4-8 4m16 0l-8 4m8-4v10l-8 4m0-10L4 7m8 4v10M4 7v10l8 4"/></svg>'.html_safe
+    when "pendiente_endoso_agente_aduanal"
+      '<svg class="w-4 h-4" fill="none" stroke="currentColor" viewBox="0 0 24 24"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 9v2m0 4h.01m-6.938 4h13.856c1.54 0 2.502-1.667 1.732-3L13.732 4c-.77-1.333-2.694-1.333-3.464 0L3.34 16c-.77 1.333.192 3 1.732 3z"/></svg>'.html_safe
+    when "pendiente_endoso_consignatario"
+      '<svg class="w-4 h-4" fill="none" stroke="currentColor" viewBox="0 0 24 24"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M16 7a4 4 0 11-8 0 4 4 0 018 0zM12 14a7 7 0 00-7 7h14a7 7 0 00-7-7z"/></svg>'.html_safe
+    when "finalizado"
+      '<svg class="w-4 h-4" fill="none" stroke="currentColor" viewBox="0 0 24 24"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z"/></svg>'.html_safe
+    when "instrucciones_pendientes"
+      '<svg class="w-4 h-4" fill="none" stroke="currentColor" viewBox="0 0 24 24"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M11 5H6a2 2 0 00-2 2v11a2 2 0 002 2h11a2 2 0 002-2v-5m-1.414-9.414a2 2 0 112.828 2.828L11.828 15H9v-2.828l8.586-8.586z"/></svg>'.html_safe
+    when "pendiente_pagos_locales"
+      '<svg class="w-4 h-4" fill="none" stroke="currentColor" viewBox="0 0 24 24"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 8c-1.657 0-3 .895-3 2s1.343 2 3 2 3 .895 3 2-1.343 2-3 2m0-8c1.11 0 2.08.402 2.599 1M12 8V7m0 1v8m0 0v1m0-1c-1.11 0-2.08-.402-2.599-1"/></svg>'.html_safe
+    else
+      '<svg class="w-4 h-4" fill="none" stroke="currentColor" viewBox="0 0 24 24"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"/></svg>'.html_safe
+    end
+  end
 end
