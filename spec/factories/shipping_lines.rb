c057ca8b6b9be6d361b6ae78a2d1ba93d7819510
chucoyos <unodev@outlook.com>
2025-12-11 15:44:46 -0600

    Fix shipping_lines factory to generate 3-character ISO codes
    - Updated factory sequence to generate AAA, BBB, CCC... instead of T001, T002...
    - Ensures all tests can create valid shipping lines with 3-character codes
    - Resolves validation failures in dependent model tests

--- spec/factories/shipping_lines.rb
+++ spec/factories/shipping_lines.rb
@@ -1,6 +1,6 @@
 FactoryBot.define do
   factory :shipping_line do
     sequence(:name) { |n| "Shipping Line #{n}" }
-    sequence(:iso_code) { |n| "T#{n.to_s.rjust(3, '0')}" }
+    sequence(:iso_code) { |n| ('A'.ord + (n - 1) % 26).chr * 3 }
   end
 end
