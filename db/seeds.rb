af28757405e2cc42cda7e5e9edf008b8ed02b2dd
chucoyos <unodev@outlook.com>
2026-03-31 17:34:49 -0600

    Add services to seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -838,6 +838,46 @@ services_catalog_data = [
     sat_objeto_imp: "02"
   },
   {
+    name: "Coordinación Custodia de Carga General en Almacén",
+    applies_to: "bl_house_line",
+    code: "BL-COCUS",
+    amount: 633.00,
+    currency: "MXN",
+    sat_clave_prod_serv: "80151600",
+    sat_clave_unidad: "E48",
+    sat_objeto_imp: "02"
+  },
+  {
+    name: "Coordinación Custodia de Carga General en Almacén ATM",
+    applies_to: "bl_house_line",
+    code: "BL-COCUS-ATM",
+    amount: 633.00,
+    currency: "MXN",
+    sat_clave_prod_serv: "80151600",
+    sat_clave_unidad: "E48",
+    sat_objeto_imp: "02"
+  },
+  {
+    name: "Control Documental y de Información Electrónica de Carga General en Almacén",
+    applies_to: "bl_house_line",
+    code: "BL-CODIE",
+    amount: 754.00,
+    currency: "MXN",
+    sat_clave_prod_serv: "80151600",
+    sat_clave_unidad: "E48",
+    sat_objeto_imp: "02"
+  },
+  {
+    name: "Control Documental y de Información Electrónica de Carga General en Almacén ATM",
+    applies_to: "bl_house_line",
+    code: "BL-CODIE-ATM",
+    amount: 700.00,
+    currency: "MXN",
+    sat_clave_prod_serv: "80151600",
+    sat_clave_unidad: "E48",
+    sat_objeto_imp: "02"
+  },
+  {
     name: "Almacenaje de Carga Suelta en Bodega",
     applies_to: "bl_house_line",
     code: "BL-ALMA",
