1e980eb9c9171561b8e49b37f792b899f2a6604d
chucoyos <unodev@outlook.com>
2026-04-15 03:25:04 -0600

    Add two more videos to tutorial section

--- app/controllers/tutorials_controller.rb
+++ app/controllers/tutorials_controller.rb
@@ -4,16 +4,28 @@ class TutorialsController < ApplicationController
   def index
     @videos = [
       {
+        title: "Solicitando Revalidación",
+        description: "Enviar una solicitud de revalidación es más sencillo que nunca.",
+        video_id: "jnvhU0_Jmmk",
+        duration: "Video 1"
+      },
+      {
+        title: "Registro de Pagos en Global DYC",
+        description: "Te explicamos el procedimiento para enviar tus comprobantes de pago.",
+        video_id: "2cqTxOh3n_4",
+        duration: "Video 2"
+      },
+      {
         title: "Cómo preparar tu flujo de trabajo",
         description: "Guía rápida para entender el proceso operativo dentro de la plataforma.",
         video_id: "Mj6YrvsIXD8",
-        duration: "Video 1"
+        duration: "Video 3"
       },
       {
         title: "Buenas prácticas de seguimiento",
         description: "Recomendaciones para mantener trazabilidad y control en cada etapa.",
         video_id: "Ffo18-zaJCI",
-        duration: "Video 2"
+        duration: "Video 4"
       }
     ]
   end
